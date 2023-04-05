16459d02a6c4d64a8c6890b758866142104d4d6d
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-05 12:38:53 -0400

    there

--- scene.py
+++ scene.py
@@ -1,15 +1,29 @@
 from manim import *
 
-class Demo(Scene):
+from manim_voiceover import VoiceoverScene
+from manim_voiceover.services.coqui import CoquiService
+
+class Demo(VoiceoverScene):
     def construct(self):
+        self.set_speech_service(CoquiService())
+        
         square = Square()
         square.rotate(PI / 4)
-        self.play(Create(square))
-        self.play(Transform(square, Circle()))
-        tex = Tex(r'\LaTeX', font_size=100)
-        text = Text(r'今日は！', font_size=100)
+
+        with self.voiceover("This can draw shapes") as tracker:
+            self.play(Create(square), run_time=tracker.duration)
+
+        with self.voiceover("interpolate stuff") as tracker:
+            self.play(Transform(square, Circle()), run_time=tracker.duration)
+        
+        tex = Tex(r"\LaTeX", font_size=100)
+        text = Text(r"今日は！", font_size=100)
         tex.next_to(square, LEFT)
         text.next_to(square, RIGHT)
-        self.play(Write(tex), Write(text))
-        self.wait(1)
+
+        with self.voiceover("and draw text.") as tracker:
+            self.play(Write(tex), Write(text), run_time=tracker.duration)
+        
+        self.wait()
         self.play(FadeOut(tex), FadeOut(text), FadeOut(square))
+        self.wait()
