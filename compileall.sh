ce2cc8db599dbc087c37817c6fdf724d6371b2ee
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-18 15:42:38 -0400

    new plan

--- compileall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Please note that this script does actually render anything. "
-echo "It just concatenates generated scenes together in the correct order. "
-
-rm output.mp4
-python makesceneorder.py
-ffmpeg -i sceneorder.txt -c copy output.mp4
