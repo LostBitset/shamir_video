d5731fd0e1f67da608800eba943dd1da9d14cd3b
Landon <102832670+LostBitset@users.noreply.github.com>
2023-04-18 16:41:49 -0400

    Small fix

--- render.sh
+++ render.sh
@@ -3,6 +3,10 @@
 echo "RENDERING FULL VIDEO!"
 echo "[render.sh] Settings: 1080p60 (-qh)"
 
+echo "[render.sh] PURGING EXISTING MEDIA"
+rm -rf media 2>/dev/null || echo "(No previous media to purge.)"
+echo "[render.sh] PURGING EXISTING MEDIA - STEP DONE"
+
 echo "[render.sh] -delegate-> [manim]"
 manim -qh -a scene.py
 echo "[render.sh] <-return- [manim]"
